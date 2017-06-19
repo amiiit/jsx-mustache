520f9668067c8a50c46f0670179d86a3c2de9891
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2017-06-19 15:25:53 +0200

    current state

---
+++ src/Ad.js
@@ -0,0 +1,25 @@
+const Title = () => {
+  <div></div>
+}
+
+const Image = () => {
+
+}
+
+const Body = () => {
+  <div>
+    <Image></Image>
+  </div>
+}
+
+const Footer = () => {
+  <div></div>
+}
+
+const Ad = () => {
+  <div>
+    <Title></Title>
+    <Body></Body>
+    <End></End>
+  </div>
+}
--- src/__tests__/ordering-test.js
+++ src/__tests__/ordering-test.js
@@ -2,9 +2,11 @@ import {parseMustacheTag, transformString, transform} from '../transformator'
 import {Loop, MustacheImage} from '../jsx-mustache'
 import React from 'react'
 
-describe('Transformator', () => {
+const SectionA = () => <div> </div>
 
-  it('Transform string', () => {
+describe('Ordering', () => {
+
+  it('Different orders', () => {
     const tag = `<div>
     <div>
         <mustachetag data-prefix="#" data-content="images"></mustachetag>
